e97d220850b1d301ac11d681d0e7d00c3210451c
Jackeastern <oostenjac@gmail.com>
2021-08-20 23:27:05 +0200

    about_flex

--- js/my_js.js
+++ js/my_js.js
@@ -1,11 +1,17 @@
 async function loadFileAndPrintToConsole(url) {
     try {
       const response = await fetch(url);
-      const data = await response.text();
+      const data_raw = await response.text();
+      const about_array = data_raw.split('\n');
+      const data = "";
+      for(var line = 0; line < about_array.length; line++){
+        data = data + "<p>" + about_array[line] + "</p>";
+      }
       document.getElementById("about_me").innerHTML = data;
       console.log(data);
     } catch (err) {
       console.error(err);
+      console.log("Check about file");
     }
   }
 loadFileAndPrintToConsole("https://jacquesoostenbrink.co.za/aboutme.txt")
